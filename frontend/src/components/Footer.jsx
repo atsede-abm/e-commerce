5e670927ae8ff6d83fb077003c85d1fa6770d24d
atsede-abm <atsedeb650@gmail.com>
2026-03-22 20:41:35 +0300

    category commit

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,17 +3,39 @@ import { assets } from "../assets/frontend_assets/assets";
 
 const Footer = () => {
     return (
+        
         <div>
-        <div className="flex flex-col sm:grid grid=cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
+        <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
 <div>
     <img src={assets.logo} className="mb-5 w-32" alt=""/>
     <p className="m-full md:w-2/3 text-gray-600"> 
-        Lorem Ipsum is simply dummy text of the printing and type setting <br/>
-        industry. Lorem Ipsum has been the industry's standard dummy text <br/>
-        ever since the 1500s. when on unknown printer took a galley of type and <br/>
+        Lorem Ipsum is simply dummy text of the printing and type setting 
+        industry. Lorem Ipsum has been the industry's standard dummy text 
+        ever since the 1500s. when on unknown printer took a galley of type and 
         scrambled it to make a type specimen book.
          </p>
    </div>
+           <div>
+            <p className="text-xl font-medium mb-5">COMPANY</p>
+            <ul className="flex flex-col gap-1 text-gray-600"> 
+                <li>Home</li>
+                <li>About us</li>
+                <li>Delivery</li>
+                <li>Privacy policy</li>
+            </ul>
+           </div>
+          <div>
+            <p className="text-x1 font-medium mb-5">GET IN TOUCH</p>
+            <ul className="flex flex-col gap-1 text-gray-600">
+               <li>+251 945083316</li>
+               <li>atsedeb650@gmail.com</li> 
+            </ul>
+          </div>
+        </div>
+
+        <div>
+            <hr/>
+            <p className="py-5 text-sm text-center">Copyright 2026 atsedeb650@gmail.com -All Right Reserved.</p>
         </div>
 
         </div>
