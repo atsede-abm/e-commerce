777040bf1fed6f118fd94c5bae572ed8582e112a
atsede-abm <atsedeb650@gmail.com>
2026-03-24 17:21:52 +0300

    search commit

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -7,7 +7,7 @@ import ProductItem from "../components/ProductItem";
 const Collection = () => {
 
 
-    const { products } = useContext(ShopContext);
+    const { products , search , showSearch } = useContext(ShopContext);
     const  [showFilter,setShowFilter] = useState(false);
     const [filterProducts,setFilterProducts] = useState([]);
     const [category,setCategory] = useState([]);
@@ -37,6 +37,10 @@ const Collection = () => {
     const applyFilter = () => {
 
       let productsCopy = products.slice();
+
+      if (showSearch && search){
+       productsCopy= productsCopy.filter(item=> item.name.toLowerCase().includes(search.toLowerCase()))
+      }
 
       if (category.length > 0){
          productsCopy = productsCopy.filter(item => category.includes(item.category))
@@ -71,8 +75,8 @@ const Collection = () => {
      }
 
     useEffect(()=>{
-       applyFilter()
-    },[category,subCategory])
+       applyFilter();
+    },[category,subCategory,search,showSearch])
 
     useEffect(()=>{
       sortProduct();
